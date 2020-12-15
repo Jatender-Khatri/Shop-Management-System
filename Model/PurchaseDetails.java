971c653d2cc2e692ce88c9e8c5e14fed2f1882aa
Jatender-Khatri <60816519+Jatender-Khatri@users.noreply.github.com>
2020-12-16 01:21:11 +0500

    Create PurchaseDetails.java

---
+++ Model/PurchaseDetails.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shop.model;
+
+/**
+ *
+ * @author Jatendar
+ */
+public class PurchaseDetails extends  SameResult{
+    private Integer purchaseDetailsId;
+    private Purchase purchase;
+    private Product product;
+    private String status,total,quantity,purchasePrice;
+
+    public Integer getPurchaseDetailsId() {
+        return purchaseDetailsId;
+    }
+
+    public void setPurchaseDetailsId(Integer purchaseDetailsId) {
+        this.purchaseDetailsId = purchaseDetailsId;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(String purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Purchase getPurchase() {
+        return purchase;
+    }
+
+    public void setPurchase(Purchase purchase) {
+        this.purchase = purchase;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
