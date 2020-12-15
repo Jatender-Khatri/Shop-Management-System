51c2e434895e57a1a425de863514bd6e254a999e
Jatender-Khatri <60816519+Jatender-Khatri@users.noreply.github.com>
2020-12-16 01:26:33 +0500

    Create User.java

---
+++ Model/User.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shop.model;
+
+/**
+ *
+ * @author Jatendar
+ */
+public class User {
+    private Integer userId;
+    private String userName, password, email,number,address;
+    private static  Role role;
+
+    public Integer getId() {
+        return userId;
+    }
+
+    public void setId(Integer id) {
+        this.userId = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public static Role getRoll() {
+        return role;
+    }
+
+    public static void setRoll(Role roll) {
+        role = roll;
+    }
+    
+}
