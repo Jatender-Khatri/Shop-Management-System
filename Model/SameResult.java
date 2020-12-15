19ff73d8374aa056069246b349d0ba6c4a5528a2
Jatender-Khatri <60816519+Jatender-Khatri@users.noreply.github.com>
2020-12-16 01:25:06 +0500

    Create SameResult.java

---
+++ Model/SameResult.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shop.model;
+
+/**
+ *
+ * @author Jatendar
+ */
+public class SameResult {
+    private Integer createdName, modifedName;
+    private String createdDate, modifiedDate;
+
+    public Integer getCreatedName() {
+        return createdName;
+    }
+
+    public void setCreatedName(Integer createdName) {
+        this.createdName = createdName;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public Integer getModifedName() {
+        return modifedName;
+    }
+
+    public void setModifedName(Integer modifedName) {
+        this.modifedName = modifedName;
+    }
+    
+    
+}
