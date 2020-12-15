2374315d646fb87f591307f76ebbf765e4b73c07
Jatender-Khatri <60816519+Jatender-Khatri@users.noreply.github.com>
2020-12-16 01:17:54 +0500

    Create Category.java

---
+++ Model/Category.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shop.model;
+
+/**
+ *
+ * @author Jatendar
+ */
+public class Category extends SameResult{
+    private Integer categoryId;
+    private String category,remarks,status;
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+}
